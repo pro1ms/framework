e0fe6e56ed53fab6259d64c5be087d3f185791de
emris <emris@no-reply>
2026-03-09 10:16:53 +0500

    fix db driver

--- db/db.go
+++ db/db.go
@@ -2,48 +2,74 @@ package db
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"time"
 
-	"github.com/jmoiron/sqlx"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Settings struct {
 	Conn            string
-	MaxIdleConns    int
-	MaxOpenConns    int
+	MaxIdleConns    int // В pgxpool это MinConns
+	MaxOpenConns    int // В pgxpool это MaxConns
 	ConnMaxLifetime time.Duration
 }
 
 type Database struct {
-	db *sqlx.DB
+	Pool *pgxpool.Pool
 }
 
 func NewDatabase(settings Settings) *Database {
-	db, err := sqlx.Connect("pgx", settings.Conn)
+	ctx := context.Background()
+
+	config, err := pgxpool.ParseConfig(settings.Conn)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse connection string: %v", err))
+	}
+
+	config.MaxConns = int32(settings.MaxOpenConns)
+	config.MinConns = int32(settings.MaxIdleConns)
+	config.MaxConnLifetime = settings.ConnMaxLifetime
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		panic(fmt.Sprintf("failed to connect to db: %v", err))
 	}
 
-	db.SetMaxOpenConns(settings.MaxOpenConns)
-	db.SetMaxIdleConns(settings.MaxIdleConns)
-	db.SetConnMaxLifetime(settings.ConnMaxLifetime)
-
-	return &Database{db: db}
+	return &Database{Pool: pool}
 }
 
-func (d *Database) Select(ctx context.Context, name string, dest any, query string, args ...any) error {
+func Select[T any](ctx context.Context, d *Database, name string, query string, args ...any) ([]T, error) {
 	ctxWithName := context.WithValue(ctx, "name", name)
-	return d.db.SelectContext(ctxWithName, dest, query, args...)
+	rows, err := d.Pool.Query(ctxWithName, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return pgx.CollectRows(rows, pgx.RowToStructByName[T])
 }
 
-func (d *Database) Get(ctx context.Context, name string, dest any, query string, args ...any) error {
+func Get[T any](ctx context.Context, d *Database, name string, query string, args ...any) (*T, error) {
 	ctxWithName := context.WithValue(ctx, "name", name)
-	return d.db.GetContext(ctxWithName, dest, query, args...)
+	rows, err := d.Pool.Query(ctxWithName, query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[T])
+	if err != nil {
+		return nil, err
+	}
+
+	return &r, nil
 }
 
-func (d *Database) Exec(ctx context.Context, name string, query string, args ...any) (sql.Result, error) {
+func (d *Database) Exec(ctx context.Context, name string, query string, args ...any) (pgconn.CommandTag, error) {
 	ctxWithName := context.WithValue(ctx, "name", name)
-	return d.db.ExecContext(ctxWithName, query, args...)
+	return d.Pool.Exec(ctxWithName, query, args...)
+}
+
+func (d *Database) Close() {
+	d.Pool.Close()
 }
